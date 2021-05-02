02a12e8c80ab49e633138fab5d998712efad8db8
adalbertopc <adalbertinho90@gmail.com>
2021-05-02 13:10:59 -0700

    feat: add login redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import { AuthProvider } from './auth/AuthContext';
+import { useReducer, useState, useEffect } from 'react';
+import { AuthContext } from './auth/AuthContext';
+import { authReducer } from './auth/authReducer';
+import { validateTokenCookie } from './auth/tokenCookie';
+import { types } from './constants/types';
 import { AppRouter } from './routes/AppRouter';
+
 function App() {
+	console.log('app render');
+	const [auth, dispatch] = useReducer(authReducer, { logged: false }, () => ({
+		logged: false,
+	}));
+
+	const checkToken = () => {
+		validateTokenCookie()
+			.then((res) => {
+				if (res) {
+					const { token, username, firstName, userType } = res;
+					dispatch({
+						type: types.login,
+						payload: { user: { token, username, firstName, userType } },
+					});
+				}
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	useEffect(() => {
+		checkToken();
+	}, []);
+
 	return (
-		<AuthProvider>
+		<AuthContext.Provider value={{ auth, dispatch }}>
 			<AppRouter />
-		</AuthProvider>
+		</AuthContext.Provider>
 	);
 }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 import { Container, FormGroup, Input, Button, Title } from '../components/FormComponents';
 import { AuthContext } from '../auth/AuthContext';
 import { useForm } from '../hooks/useForm';
+import { types } from '../constants/types';
+import { Redirect } from 'react-router';
 
 export const LoginForm = () => {
-	const { setAuthData } = useContext(AuthContext);
+	const { auth, dispatch } = useContext(AuthContext);
 	const [values, handleInputChanges] = useForm({});
 	const [error, setError] = useState({ error: false, message: '' });
 
@@ -15,14 +17,19 @@ export const LoginForm = () => {
 			const res = await axios.post('http://localhost:3000/api/user/auth', values);
 			const data = res.data;
 			if (!data.error && data !== null) {
-				setAuthData({ logged: true, ...data.data });
+				dispatch({ type: types.login, payload: { ...data.data } });
 				document.cookie = `token=${data.data.token}`;
 			}
 		} catch (e) {
-			const { error, message } = e.response.data;
-			setError({ error, message });
+			console.log(e);
+			// const { error, message } = e.response.data;
+			// setError({ error, message });
 		}
 	};
+
+	if (auth.logged) {
+		return <Redirect to='/dashboard' />;
+	}
 
 	return (
 		<Container>
